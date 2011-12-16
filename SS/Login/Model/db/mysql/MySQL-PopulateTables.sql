cd736c50864214632c6f68cc8ada257af3052340
maxweld <dylan.maxwell@lightsource.ca>
2011-12-16 14:31:54 -0600

    Add 'ADMIN_VESPERS' as default role in populate DB script.

--- SS/Login/Model/db/mysql/MySQL-PopulateTables.sql
+++ SS/Login/Model/db/mysql/MySQL-PopulateTables.sql
@@ -9,7 +9,8 @@
 -- --------------------- Default Login Roles --------------------------------
 insert into login_role (name) values ('EVERYBODY'); 		-- id: 1
 insert into login_role (name) values ('ADMIN_PROJECTS'); 	-- id: 2
-insert into login_role (name) values ('ADMIN_VESPERS'); -- id: 3
+insert into login_role (name) values ('ADMIN_VESPERS');		-- id: 3
+insert into login_role (name) values ('ADMIN_SESSIONS');	-- id: 4
 
 -- --------------------- Default Login Groups --------------------------------
 insert into login_group (name) values ('EVERYBODY'); 		-- id: 1
@@ -19,3 +20,4 @@ insert into login_group (name) values ('ADMINISTRATORS'); 	-- id: 2
 insert into login_group_role (login_role_id, login_group_id) values (1, 1); -- id: 1
 insert into login_group_role (login_role_id, login_group_id) values (2, 2); -- id: 2
 insert into login_group_role (login_role_id, login_group_id) values (3, 2); -- id: 3
+insert into login_group_role (login_role_id, login_group_id) values (4, 2); -- id: 4
