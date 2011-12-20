ad70984a9b164cb550574c1f47574caf09ee1d08
maxweld <dylan.maxwell@lightsource.ca>
2011-12-20 14:34:35 -0600

    Correct Canadian Light Source facility and add technique 'XRD'.

--- SS/Core/Model/db/mysql/MySQL-PopulateTables.sql
+++ SS/Core/Model/db/mysql/MySQL-PopulateTables.sql
@@ -11,7 +11,8 @@
 -- Facility
 -- =========================================================================================
 insert into facility (name, long_name, description, phone_number, email_address, location, authc_url, home_url)
-	values ('CLSI', 'Canadian Light Source', '', '(306) 657-3500', '', 'Saskatoon, Saskatchewan, Canada', 'https://cas.lightsource.ca/cas', 'http://sciencestudio.lightsource.ca/ss');
+	values ('CLS', 'Canadian Light Source', '', '(306) 657-3500', '', 'Saskatoon, Saskatchewan, Canada', 'https://cas.lightsource.ca/cas', 'http://sciencestudio.lightsource.ca/ss');
+
 
 -- =========================================================================================
 -- Laboratory
@@ -19,18 +20,22 @@ insert into facility (name, long_name, description, phone_number, email_address,
 insert into laboratory (facility_id, name, long_name, description, phone_number, email_address, view_url)
 	values (1, 'VESPERS', 'VESPERS', '(306) 657-3620', '', '', '/ssvespers/beamline/main.html');
 
+	
 -- =========================================================================================
 -- Instrument
 -- =========================================================================================
 insert into instrument (laboratory_id, name, long_name, description)
 	values (1, 'Microprobe', 'Microprobe Endstation', '');
 
+
 -- =========================================================================================
 -- Technique
 -- =========================================================================================
 insert into technique (name, long_name, description) values ('XRF', 'X-Ray Fluorescence', '');
+insert into technique (name, long_name, description) values ('XRD', 'X-Ray Diffraction', '');
 
 -- =========================================================================================
 -- Instrument Technique
 -- =========================================================================================
 insert into instrument_technique (instrument_id, technique_id) values (1,1);
+insert into instrument_technique (instrument_id, technique_id) values (1,2);
