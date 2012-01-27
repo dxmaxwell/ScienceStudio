8390b127c9a63c9eaec3c65fcb3cdf5b4523d467
Nathaniel Sherry <nathaniel.sherry@gmail.com>
2012-01-27 15:38:44 -0500

    Added start of documentation script/data

---
+++ SS/Core/Rest/WebApp/WebContent/docs/gen.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/ruby
+
+require 'json'
+require 'erb'
+require 'cgi'
+
+
+def getObjectDocs
+
+	return `ls *.json`.split("\n").map{|file|
+
+		begin
+
+			JSON.parse(`cat #{file}`)
+					
+		rescue => e
+			$stderr.puts file
+			$stderr.puts e
+			nil
+		end
+	}.select{|doc| doc != nil }
+
+end
+
+
+
+template = `cat template.html`.gsub(/^\s*\%/, '%')
+
+erb = ERB.new(template, 0, "%<>")
+
+begin
+	page = erb.result
+rescue => e
+	$stderr.puts e
+end
+
+puts page
